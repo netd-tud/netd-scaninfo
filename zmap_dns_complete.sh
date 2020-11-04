f5f3aebae75b6523a28679322a83a5b63b4f0957
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2020-11-04 13:28:41 +0100

    zmap and dumpcap

---
+++ zmap_dns_complete.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# config
+zmap="/home/skyo/Repositories/zmap/sbin/zmap"
+request="A,google.com"
+logfile="zmap_dns_complete.log"
+interface="eno1"
+node_ip="141.22.28.227"
+
+# run zmap
+sudo $zmap \
+    --interface="$interface" \
+    --source-ip="$node_ip" \
+    --target-port=53 \
+    --probe-module=dns \
+    --probe-args="$request" \
+    --seed=13371337 \
+    --blocklist-file=/etc/zmap/blocklist.conf \
+    --rate=25000 \
+    -O json -o $logfile
