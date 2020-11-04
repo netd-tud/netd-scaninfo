270102efb5cea8d146fba0d62b5116d73f8f0254
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2020-11-04 19:12:27 +0100

    ip check for correct response

--- tshark_sanitize_ip_src.sh
+++ tshark_sanitize_ip_src.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 domain="dnsscan.shadowserver.org"
+ip="184.105.143.133"
 dns_sanitize="ip and not icmp and udp and udp.srcport==53 and dns"
 dns_sanitize+=" and dns.qry.name==${domain} and dns.flags.response==1"
 dns_sanitize+=" and dns.flags.rcode==0 and dns.flags.recavail==1"
+dns_sanitize+=" and dns.a==${ip}"
 
 tshark -nr $1 -Y "$dns_sanitize" -T fields -e ip.src
