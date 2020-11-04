0e3f106022ec5b16dcd63bfd9d747f140dea57b0
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2020-11-04 13:43:21 +0100

    dumpcap script with capture filter instead of display filter

--- tshark_dns_responses.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# config
-interface="eno1"
-time_rot="300"
-
-# rotate every 5 minutes, only record dns responses (and not ICMP backscatter)
-
-sudo tshark -ni any -T fields -e "ip.src" -e "udp.srcport" -e "ip.dst" -e "udp.dstport" -e "ip.id" -e "dns.id" -e "dns.qry.name" -e "dns.flags" -e "dns.resp.ttl" -Y "dns and dns.qry.name==google.com"
