6dceb4436200dc7cdb22ee43577a286a8fdb7ecb
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2020-11-04 17:57:06 +0100

    update sanitization for shadow

--- tshark_sanitize_ip_src.sh
+++ tshark_sanitize_ip_src.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+domain="dnsscan.shadowserver.org"
 dns_sanitize="ip and not icmp and udp and udp.srcport==53 and dns"
-dns_sanitize+=" and dns.qry.name==google.com and dns.flags.response==1"
+dns_sanitize+=" and dns.qry.name==${domain} and dns.flags.response==1"
 dns_sanitize+=" and dns.flags.rcode==0 and dns.flags.recavail==1"
 
 tshark -nr $1 -Y "$dns_sanitize" -T fields -e ip.src
