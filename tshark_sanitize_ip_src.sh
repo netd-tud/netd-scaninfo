8d005df9d05ae37c547141a403381ae2f6a22b07
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2020-11-04 16:15:09 +0100

    beautify filter

--- tshark_sanitize_ip_src.sh
+++ tshark_sanitize_ip_src.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dns_sanitize="ip and not icmp and udp and udp.srcport==53 and dns"
-dns_sanitize+=" and dns.flags.response==1 and dns.qry.name==google.com"
+dns_sanitize+=" and dns.qry.name==google.com and dns.flags.response==1"
 dns_sanitize+=" and dns.flags.rcode==0 and dns.flags.recavail==1"
 
 tshark -nr $1 -Y "$dns_sanitize" -T fields -e ip.src
