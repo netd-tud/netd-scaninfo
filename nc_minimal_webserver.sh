e010817d096b93d5a1606153f44cb4d0d86649ed
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2020-11-05 11:44:49 +0100

    new website

--- nc_minimal_webserver.sh
+++ nc_minimal_webserver.sh
@@ -5,7 +5,7 @@
 #   https://en.wikipedia.org/wiki/HTTP_303#Example
 
 server_port="80"
-http_url="https://marcinnaw.github.io/scans-ilab/"
+http_url="https://ilab-scans.realmv6.org/"
 http_redirect="HTTP/1.1 303 See Other\r\nLocation: $http_url"
 
 while true; do
