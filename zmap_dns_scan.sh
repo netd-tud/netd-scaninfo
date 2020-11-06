a608bc6b046c04fdcf8dfa94b86e0676f09a687f
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2020-11-06 16:36:39 +0100

    shuf ocmmand

--- zmap_dns_scan.sh
+++ zmap_dns_scan.sh
@@ -36,15 +36,16 @@ if [[ "$mode" == "complete" ]]; then
         --output-filter="success = 1 && app_success = 1"
 
 elif [[ "$mode" == "limited" ]]; then
+    # we might have to decrease log level, many txt len warnings
     zmap \
         --target-port=53 \
         --probe-module=dns \
         --probe-args="$dns_request_limited" \
         --blocklist-file=/etc/zmap/blocklist.conf \
-        --rate=25000 \
+        --rate=15000 \
         -O json -o $logfile \
         --output-fields="saddr,success,app_success" \
-        --allowlist-file=<(zcat amp_src.txt.gz | shuffle)
+        --allowlist-file=<(zcat amp_src.txt.gz | shuf)
 
 else
     echo "unknown scan mode"
