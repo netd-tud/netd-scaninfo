7b0c895b691b298d5f96968c2cefcd6858016772
Marcin Nawrocki <Marcin.Nawrocki@fu-berlin.de>
2020-12-01 16:30:47 +0100

    new measurement count

--- merge_amplifier_info.sh
+++ merge_amplifier_info.sh
@@ -1,17 +1,19 @@
-#!/bin/bash
+#!bin/bash
+
+cnt="3"  # used for file tags
+pcap_dir="pcaps_dns_complete/qry-a-shadowserver.org-v${cnt}"
+json_file="json_dns_complete/zmap_dns_complete_1606753012_third_complete_scan.log"
 
 # read all ips from the pcap
-pcap_dir="pcaps_dns_complete"
-for pcap in $(realpath "${pcap_dir}/"*".pcap" | head -n -2)
+for pcap in $(realpath "${pcap_dir}/"*".pcap")
 do
     ./tshark_sanitize_ip_src.sh $pcap
-done | sort | uniq | gzip > "pcap_sources.txt.gz"
+done | sort | uniq | gzip > "pcap_sources_s${cnt}.txt.gz"
 
 # read all ips from the zmap json
-json_file="json_dns_complete/zmap_dns_complete_1604505830.log"
 cat $json_file | jq ."saddr" | sed -e 's/^"//' -e 's/"$//' |\
-    sort | uniq | gzip > "json_sources.txt.gz"
+    sort | uniq | gzip > "json_sources_s${cnt}.txt.gz"
 
 # find common sources
-comm -12 <(zcat pcap_sources.txt.gz) <(zcat json_sources.txt.gz) |\
-    sort | gzip > "amp_src.txt.gz"
+comm -12 <(zcat pcap_sources_s${cnt}.txt.gz) <(zcat json_sources_s${cnt}.txt.gz) |\
+    sort | gzip > "amp_src_s${cnt}.txt.gz"
